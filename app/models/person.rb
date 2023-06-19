4391b903e09c268bd350c003bdab76625334e182
xuanduc <nxduc0703@gmail.com>
2023-06-19 22:16:49 +0700

    validate and callbacks

---
+++ app/models/person.rb
@@ -0,0 +1,12 @@
+class Person < ApplicationRecord
+  validates :name, :age, presence: true
+  validates_with MyValidator
+  
+  before_create :message 
+  
+  def message
+    puts "you will create a model"
+  end
+  
+end
+
